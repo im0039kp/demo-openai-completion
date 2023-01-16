aa50068fdd69c2f2da3d4665ac642f719dba0616
im0039kp <im0039kp@gmail.com>
2023-01-16 16:39:16 +0900

    パラメーターをPlaygroundと同じ値にする、

--- chat.py
+++ chat.py
@@ -11,11 +11,11 @@ prompt = "こんにちは、自己紹介してください。"
 response = openai.Completion.create(
   model='text-davinci-003',
   prompt=prompt,
-  temperature=0, # ランダム性の制御[0-1]
-  max_tokens=1000, # 返ってくるレスポンストークンの最大数
-  top_p=1.0, # 多様性の制御[0-1]
-  frequency_penalty=0.0, # 周波数制御[0-2]：高いと同じ話題を繰り返さなくなる
-  presence_penalty=0.0 # 新規トピック制御[0-2]：高いと新規のトピックが出現しやすくなる
+  temperature=0.4,
+  max_tokens=64,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
 )
 
 texts = ''.join([choice['text'] for choice in response.choices])
--- chat.ts
+++ chat.ts
@@ -13,11 +13,11 @@ openai
   .createCompletion({
     model: "text-davinci-003",
     prompt: text,
-    temperature: 0, // ランダム性の制御[0-1]
-    max_tokens: 1000, // 返ってくるレスポンストークンの最大数
-    top_p: 1.0, // 多様性の制御[0-1]
-    frequency_penalty: 0.0, // 周波数制御[0-2]：高いと同じ話題を繰り返さなくなる
-    presence_penalty: 0.0, // 新規トピック制御[0-2]：高いと新規のトピックが出現しやすくなる
+    temperature: 0.4,
+    max_tokens: 64,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
   })
   .then((completion) => {
     const choices = completion.data.choices[0];
